c8d5dd2cab015eb48f720ccf54c4cb8de0db8564
Kristy Corkan <kcorkan@rallydev.com>
2017-10-19 08:27:11 -0600

    Update syncGithubIssues_TIMER.js

--- syncGithubIssues_TIMER.js
+++ syncGithubIssues_TIMER.js
@@ -1,17 +1,24 @@
 var baseUrl = "http://localhost:8080/rest/default/ghbrly/v1/";
 
-var url = baseUrl + "syncGithubIssues";
+var url = baseUrl + "githubIssue";
 var authHeaders = {
-    "headers":{"Authorization": "CALiveAPICreator TimerKey:1"}
+    "headers":{"Authorization": "CALiveAPICreator gitrlyAdminKey:1"}
 };
 
-/* Todo - Max has a better way to get the last time something ran */
-var sinceDate = moment(new Date());
-sinceDate.subtract(5,'minutes');
+var lastRun = '';
+
 var params = {
-    lastRun: sinceDate.toISOString()
+    "since": lastRun,
+    "filter":"all",
+    "state":"open"
 };
 
-log.debug('params' + JSON.stringify(params));
-var resp = timerUtil.restGet(url,params,authHeaders);
-log.debug('response ' + resp);
+var recent_issues = SysUtility.restGet(baseUrl + 'githubIssue', params, authHeaders);
+recent_issues = JSON.parse(recent_issues);
+
+if (recent_issues && recent_issues.length > 0){
+    /* this will do a merge insert so it will insert new ones only */
+    var objectLinkUrl = baseUrl + "github_object";
+    SysUtility.restPut(objectLinkUrl,{},authHeaders,recent_issues);
+} 
+log.debug(recent_issues.length + ' issues found.');
